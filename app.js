a7017597cc2cbc76131abde62ff72e07dcf424b1
SaiPavan N <SaiPavan N>
2016-11-07 21:27:48 +0530

    final version

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 app.use(express.static(__dirname + '/public'));
+app.use('/bower_components',  express.static(__dirname + '/bower_components'));
 
 app.get('/',function(req,res){
     res.sendfile('index.html');
